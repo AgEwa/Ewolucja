06ba67f85099f869454b1f810a0b2f17ec83446a
Michal <legczylin.michal@gmail.com>
2024-08-20 21:26:59 +0200

    Added descriptions to some functions and TODOs

--- src/population/Specimen.py
+++ src/population/Specimen.py
@@ -27,91 +27,100 @@ class Specimen:
 
         return
 
-    def think(self, step):
+    def think(self, p_step: int) -> dict[ActionType, float]:
         """ returns dict of ActionType key : float value """
 
-        return self.brain.run(step)
+        assert isinstance(p_step, int) and p_step >= 0
 
-    def act(self, actions):
-        if ActionType.SET_RESPONSIVENESS in actions:
-            level = actions[ActionType.SET_RESPONSIVENESS]
+        return self.brain.run(p_step)
+
+    def act(self, p_actions: dict[ActionType, float]) -> None:
+        """ acts based on passed actions and their activation level values """
+
+        assert isinstance(p_actions, dict)
+        for key, value in p_actions.items():
+            assert isinstance(key, ActionType)
+            assert isinstance(value, float)
+
+        if ActionType.SET_RESPONSIVENESS in p_actions:
+            level = p_actions[ActionType.SET_RESPONSIVENESS]
             level = squeeze(level)
             self.responsiveness = level
 
         responsiveness_adj = response_curve(self.responsiveness)
 
         # really don't know what logic is behind this, adapted from source
-        if ActionType.SET_OSCILLATOR_PERIOD in actions:
-            period = actions[ActionType.SET_OSCILLATOR_PERIOD]
+        if ActionType.SET_OSCILLATOR_PERIOD in p_actions:
+            period = p_actions[ActionType.SET_OSCILLATOR_PERIOD]
             period = squeeze(period)
             period = 1 + int(1.5 + math.exp(7 * period))
 
             if 2 <= period <= 2048:
                 self.osc_period = period
 
-        if ActionType.SET_LONGPROBE_DIST in actions:
+        if ActionType.SET_LONGPROBE_DIST in p_actions:
             max_long_probe_dist = 32
-            level = actions[ActionType.SET_LONGPROBE_DIST]
+            level = p_actions[ActionType.SET_LONGPROBE_DIST]
             level = squeeze(level)
             level = 1 + level * max_long_probe_dist
             self.long_probe_dist = int(level)
 
-        if ActionType.EMIT_PHEROMONE in actions:
+        if ActionType.EMIT_PHEROMONE in p_actions:
             emit_threshold = 0.5
 
-            level = actions[ActionType.EMIT_PHEROMONE]
+            level = p_actions[ActionType.EMIT_PHEROMONE]
             level = squeeze(level)
             level *= responsiveness_adj
 
             if level > emit_threshold and probability(level):
-                # implement pheromones
+                # TODO: implement pheromones
 
                 pass
 
-        if ActionType.KILL in actions and config.KILL_ENABLED:
+        if ActionType.KILL in p_actions and config.KILL_ENABLED:
             kill_threshold = 0.5
 
-            level = actions[ActionType.KILL]
+            level = p_actions[ActionType.KILL]
             level = squeeze(level)
             level *= responsiveness_adj
 
             if level > kill_threshold and probability(level):
-                # implement kill
+                # TODO: implement kill
 
                 pass
 
         # specimen's last movement as x and y direction
         last_move_offset = Conversions.direction_as_normalized_coord(self.last_movement_direction)
         # retrieve x movement
-        move_x = 0 if ActionType.MOVE_X not in actions else actions[ActionType.MOVE_X]
+        move_x = 0 if ActionType.MOVE_X not in p_actions else p_actions[ActionType.MOVE_X]
         # retrieve y movement
-        move_y = 0 if ActionType.MOVE_Y not in actions else actions[ActionType.MOVE_Y]
+        move_y = 0 if ActionType.MOVE_Y not in p_actions else p_actions[ActionType.MOVE_Y]
         # retrieve east movement
-        move_x += 0 if ActionType.MOVE_EAST not in actions else actions[ActionType.MOVE_EAST]
+        move_x += 0 if ActionType.MOVE_EAST not in p_actions else p_actions[ActionType.MOVE_EAST]
         # retrieve west movement
-        move_x -= 0 if ActionType.MOVE_WEST not in actions else actions[ActionType.MOVE_WEST]
+        move_x -= 0 if ActionType.MOVE_WEST not in p_actions else p_actions[ActionType.MOVE_WEST]
         # retrieve north movement
-        move_y += 0 if ActionType.MOVE_NORTH not in actions else actions[ActionType.MOVE_NORTH]
+        move_y += 0 if ActionType.MOVE_NORTH not in p_actions else p_actions[ActionType.MOVE_NORTH]
         # retrieve south movement
-        move_y -= 0 if ActionType.MOVE_SOUTH not in actions else actions[ActionType.MOVE_SOUTH]
+        move_y -= 0 if ActionType.MOVE_SOUTH not in p_actions else p_actions[ActionType.MOVE_SOUTH]
 
         # continue last direction movement
-        if ActionType.MOVE_FORWARD in actions:
-            level = actions[ActionType.MOVE_FORWARD]
+        if ActionType.MOVE_FORWARD in p_actions:
+            level = p_actions[ActionType.MOVE_FORWARD]
 
             move_x += last_move_offset.x * level
             move_y += last_move_offset.y * level
 
         # turn around of last direction movement
-        if ActionType.MOVE_REVERSE in actions:
-            level = actions[ActionType.MOVE_REVERSE]
+        if ActionType.MOVE_REVERSE in p_actions:
+            level = p_actions[ActionType.MOVE_REVERSE]
 
             move_x -= last_move_offset.x * level
             move_y -= last_move_offset.y * level
 
         # turn left respective to last direction movement
-        if ActionType.MOVE_LEFT in actions:
-            level = actions[ActionType.MOVE_LEFT]
+        if ActionType.MOVE_LEFT in p_actions:
+            level = p_actions[ActionType.MOVE_LEFT]
 
             offset = Conversions.direction_as_normalized_coord(self.last_movement_direction.rotate_90_deg_ccw())
 
@@ -119,8 +128,8 @@ class Specimen:
             move_y += offset.y * level
 
         # turn right respective to last direction movement
-        if ActionType.MOVE_RIGHT in actions:
-            level = actions[ActionType.MOVE_RIGHT]
+        if ActionType.MOVE_RIGHT in p_actions:
+            level = p_actions[ActionType.MOVE_RIGHT]
 
             offset = Conversions.direction_as_normalized_coord(self.last_movement_direction.rotate_90_deg_cw())
 
@@ -128,8 +137,8 @@ class Specimen:
             move_y += offset.y * level
 
         # move at random direction
-        if ActionType.MOVE_RANDOM in actions:
-            level = actions[ActionType.MOVE_RANDOM]
+        if ActionType.MOVE_RANDOM in p_actions:
+            level = p_actions[ActionType.MOVE_RANDOM]
 
             offset = Conversions.direction_as_normalized_coord(Direction.random())
 
@@ -143,22 +152,27 @@ class Specimen:
         # apply adjusted responsiveness
         move_x *= responsiveness_adj
         move_y *= responsiveness_adj
-        #
+        # treat absolute value of move_* as probability of movement along axis. Convert it to int so specimen moves no more than 1 tile away
         prob_x = int(probability(abs(move_x)))
         prob_y = int(probability(abs(move_y)))
-
+        # sign to decide which direction
         sign_x = -1 if move_x < 0 else 1
         sign_y = -1 if move_y < 0 else 1
 
+        # compose offset
         offset = Coord(sign_x * prob_x, sign_y * prob_y)
-
+        # calculate new location
         new_location = self.location + offset
 
+        # if new location is accessible
         if grid.in_bounds(new_location) and grid.is_empty_at(new_location):
+            # add movement to movement queue
             move_queue.append((self, new_location))
+
+        return
 
     def __str__(self):
         return f'{self.location} {self.genome}'
 
     def __repr__(self):
-        return f'{self.location} {self.genome}'
+        return self.__str__()
